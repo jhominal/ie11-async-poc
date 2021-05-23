24481b0d7741c9514180251b0b152e55258444c2
Jean Hominal <jhominal@gmail.com>
2021-05-23 23:28:22 +0200

    fix: replace www.google.com with petstore API call to avoid CORS block

--- src/poc.ts
+++ src/poc.ts
@@ -3,7 +3,7 @@ let hello: string = "hello!";
 console.log(hello);
 
 async function getGoogleStatus() : Promise<number> {
-    let response = await fetch("https://www.google.com");
+    let response = await fetch("https://petstore.swagger.io/v2/pet/findByStatus?status=available");
     return response.status;
 }
 
